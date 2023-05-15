8d0977c4e59b1b947985bd19a9a2a0d72b1f8db0
nada <nadaselim3333@gmail.com>
2023-05-15 20:52:12 +0300

    Used chat gbt to optimize a function, screen shot provided

--- board.py
+++ board.py
@@ -21,46 +21,62 @@ def add_piece(board, column, player_num):
         for i in range(ROW_COUNT):      # loops over all rows at specified column until it finds an empty cell
             if board[i][column] == 0:
                 board[i][column] = player_num
-                break
+                return i, column        # returns the position in which the piece was added as a tuple
   
 # Function that prints a board in its correct connect4 orientation  
 def print_board(board):   
     print(np.flip(board, 0))   #flips the matrix around the x axis
     
 # Function that checks if a player won after every move
-def winning_move(board, piece):
-    for c in range(COLUMN_COUNT - 3):
-        for r in range(ROW_COUNT):
-            # Check horizontal and positively sloped diagonals
-            if (
-                board[r][c] == piece
-                and board[r][c + 1] == piece
-                and board[r][c + 2] == piece
-                and board[r][c + 3] == piece
-            ) or (
-                r < ROW_COUNT - 3
-                and board[r][c] == piece
-                and board[r + 1][c + 1] == piece
-                and board[r + 2][c + 2] == piece
-                and board[r + 3][c + 3] == piece
-            ):
-                return True
+def winning_move(board, piece, last_row, last_col):
+    
+    # the (x,y) for every direction represent how I move in the board to check a win in that direction
+    directions = [
+        (0, 1),   # Horizontal so move along the x-axis, y = 0
+        (1, 0),   # Vertical so move along the y-axis, x = 0
+        (1, 1),   # Diagonal so move along the line x = y
+        (-1, 1)   # Anti-Diagonal so move along the line x = -y or -x = y
+    ]
 
-    for c in range(COLUMN_COUNT):
-        for r in range(ROW_COUNT - 3):
-            # Check vertical and negatively sloped diagonals
-            if (
-                board[r][c] == piece
-                and board[r + 1][c] == piece
-                and board[r + 2][c] == piece
-                and board[r + 3][c] == piece
-            ) or (
-                r >= 3
-                and board[r][c] == piece
-                and board[r - 1][c + 1] == piece
-                and board[r - 2][c + 2] == piece
-                and board[r - 3][c + 3] == piece
-            ):
-                return True
+    # every time this for loop iterates it checks for one of the directions starting from the last piece added
+    for dr, dc in directions:
+        count = 1      # because we have the last piece added then count = 1 and if it can reach 4 then it's a win
+        r, c = last_row, last_col
+
+        # Check in the positive direction
+        while True:
+            # adds the the values for the direction being checked to the position of the last piece
+            # so we are moving along the line for each direction with every while loop iteration
+            r += dr
+            c += dc
+            # and as long as the position we are checking
+            # its row position between 0 and 6 and column position between 0 and 7 then we are within our board parameters
+            # AND its holding a piece that matches the last added piece
+            # Then continue and count of connected pieces is incremented by 1
+            # else break the loop and enter the next while loop
+            if not (0 <= r < ROW_COUNT and 0 <= c < COLUMN_COUNT) or board[r][c] != piece:
+                break
+
+            count += 1
+
+        r, c = last_row, last_col
+
+        # Check in the negative direction, so it's along the same line for that direction just moves backward
+        while True:
+            r -= dr
+            c -= dc
+            # and as long as the position we are checking
+            # its row position between 0 and 6 and column position between 0 and 7 then we are within our board parameters
+            # AND its holding a piece that matches the last added piece
+            # Then continue and count of connected pieces is incremented by 1
+            # else break the loop 
+            if not (0 <= r < ROW_COUNT and 0 <= c < COLUMN_COUNT) or board[r][c] != piece:
+                break
+
+            count += 1
+        # checks to see if count is 4 or more so we have connected 4 pieces and the player with these pieces won
+        if count >= 4:
+            return True
+        # if not for loop executes another time to search along a different line or all direction are done so return false
 
     return False
--- connect4.py
+++ connect4.py
@@ -11,13 +11,19 @@ while not game_over:
     if turn == 1:
         column = int(input("select from 0-6: "))
         if(bd.is_valid_column(board, column)):    #checks to see if there is an empty space
-            bd.add_piece(board, column, 1)
+            row_column = bd.add_piece(board, column, 1)   # adds piece and stores the place it was added
+            if(bd.winning_move(board, 1, row_column[0], row_column[1])):
+                print("Player one won !!!")
+                game_over = True
         turn = 2    # changes turn so it's player 2
     
     # player 2 turn
     else:
         column = int(input("select from 0-6: "))
         if(bd.is_valid_column(board, column)):    #checks to see if there is an empty space
-            bd.add_piece(board, column, 2)
+            row_column = bd.add_piece(board, column, 2)   # adds piece and stores the place it was added
+            if(bd.winning_move(board, 2, row_column[0], row_column[1])):
+                print("Player two won !!!")
+                game_over = True
         turn = 1     #changes turn so it's player 1
     bd.print_board(board)
